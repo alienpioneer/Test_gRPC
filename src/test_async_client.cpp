b087c4c06ebab392b7ef23354f40ce3415f321fa
Alex <alienpioneer@gmail.com>
2024-02-01 16:14:48 +0100

    Sync_working

---
+++ src/test_async_client.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include "test.grpc.pb.h"
+
+class TestClient {
+public:
+    TestClient(std::shared_ptr<grpc::Channel> channel)
+        : m_grpc_stub(test::TestService::NewStub(channel)) {}
+
+    std::string rpcTest(const std::string user)
+    {
+        // Data we are sending to the server.
+        test::TestRequest request;
+        request.set_name(user);
+
+        // Container for the data we expect from the server.
+        test::TestReply reply;
+
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        grpc::ClientContext context;
+
+        // The actual RPC.
+        grpc::Status status = m_grpc_stub->TestMessage(&context, request, &reply);
+
+        // Act upon its status.
+        if (status.ok())
+        {
+            return reply.message();
+        }
+        else
+        {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<test::TestService::Stub> m_grpc_stub;
+};
+
+int main(int argc, char** argv)
+{
+    std::string target_str = "127.0.0.1:50051";
+    std::cout << "Target: " << target_str << std::endl;
+
+    TestClient client(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+
+    std::string reply = client.rpcTest("test_async_client");
+    std::cout << "Received reply: " << reply << std::endl;
+
+    std::getchar();
+
+    return 0;
+}
---
+++ src/tests_async_server.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include "test.grpc.pb.h"
+
+class ServiceImpl final : public test::TestService::Service
+{
+	virtual ::grpc::Status TestMessage(grpc::ServerContext* context, const test::TestRequest* request, test::TestReply* response) override
+	{
+		std::cout << "Received request from " << request->name();
+		std::string prefix("Hello ");
+		response->set_message(prefix + request->name());
+		return grpc::Status::OK;
+	}
+};
+
+void RunServer() {
+	std::string server_address = "127.0.0.1:50051";
+	ServiceImpl service;
+
+	grpc::EnableDefaultHealthCheckService(true);
+	grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+
+	grpc::ServerBuilder builder;
+	// Listen on the given address without any authentication mechanism.
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	// Register "service" as the instance through which we'll communicate with
+	// clients. In this case it corresponds to an *synchronous* service.
+	builder.RegisterService(&service);
+
+	// Finally assemble the server.
+	std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+	std::cout << "Server listening on " << server_address << std::endl;
+
+	// Wait for the server to shutdown. Note that some other thread must be
+	// responsible for shutting down the server for this call to ever return.
+	server->Wait();
+}
+
+int main(int argc, char** argv) {
+	RunServer();
+	return 0;
+}
