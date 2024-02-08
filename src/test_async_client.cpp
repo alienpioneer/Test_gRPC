a5c19cf07a6666565c44e585d2439313e5d4a9e8
Alex <alienpioneer@gmail.com>
2024-02-08 21:21:39 +0100

    Add connection checker

--- src/test_async_client.cpp
+++ src/test_async_client.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <array>
 
 #include <grpcpp/grpcpp.h>
 #include "test.grpc.pb.h"
 
 std::string server_addr = "127.0.0.1:50051";
 
-class TestClient {
+class TestClient 
+{
 public:
     TestClient(std::shared_ptr<grpc::Channel> channel)
         : m_grpc_stub(test::TestService::NewStub(channel)) {}
@@ -53,6 +55,29 @@ public:
         }
     }
 
+    static void checkConnection(std::shared_ptr<grpc::Channel> connection)
+    {
+        grpc_connectivity_state connectionState = GRPC_CHANNEL_IDLE;
+        grpc_connectivity_state connectionPrevState = GRPC_CHANNEL_TRANSIENT_FAILURE;
+
+        std::array<std::string, 5> connectionToString = { "GRPC_CHANNEL_IDLE",
+                                                        "GRPC_CHANNEL_CONNECTING",
+                                                        "GRPC_CHANNEL_READY",
+                                                        "GRPC_CHANNEL_TRANSIENT_FAILURE",
+                                                        "GRPC_CHANNEL_SHUTDOWN" };
+
+        while (connectionState != GRPC_CHANNEL_SHUTDOWN)
+        {
+            connectionState = connection->GetState(false);
+
+            if (connectionPrevState != connectionState)
+            {
+                std::cout << "Connection state : " << connectionToString[connectionState] << "\n";
+                connectionPrevState = connectionState;
+            }
+        }
+    }
+
 private:
     struct AsyncReply 
     {
@@ -68,19 +93,23 @@ private:
 
 int main(int argc, char** argv)
 {
-    std::cout << "Connecting to : " << server_addr << std::endl;
+    std::shared_ptr<grpc::Channel> connection = grpc::CreateChannel(server_addr, grpc::InsecureChannelCredentials());
 
-    TestClient client(grpc::CreateChannel(server_addr, grpc::InsecureChannelCredentials()));
+    std::cout << "Connecting to : " << server_addr << "\n";
 
-    // Spawn reader thread that loops indefinitely
+    TestClient client(connection);
+
     std::thread mThread = std::thread(&TestClient::processQueue, &client);
+    std::thread cThread = std::thread(&TestClient::checkConnection, connection);
 
-    for (auto i = 1; i < 20; i++)
+    for (auto i = 1; i < 5; i++)
     {
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
         client.rpcTest("test_async_client"+std::to_string(i));
     }
     
     mThread.join();
+    cThread.join();
 
     return 0;
 }
